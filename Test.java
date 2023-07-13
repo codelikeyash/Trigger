a433ce5307406b65c726713af41f8293c61e8db1
Work <personal@yash-IdeaPad-Gaming-3-15IMH05>
2023-07-13 16:43:44 +0530

    cs

--- Test.java
+++ Test.java
@@ -12,8 +12,8 @@ public class Test extends Source{
             System.out.println("Test Case Passed\n");
         if(s.addNum(5, 4) == 9)
             System.out.println("Test Case Passed\n");
-            
-        assert (s.addNum(2, 2) == 5);
+
+        assert (s.addNum(2, 3) == 5);
     }
     
 
